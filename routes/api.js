24384b1472d4ef24b7cebe66fca8c856d72fa06e
Awais Rafiq <awaisrafeek@gmail.com>
2020-10-19 23:45:24 +0500

    Updated postman collections, Remove book controller/model, delete readme & license

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,10 @@
 var express = require("express");
 var authRouter = require("./auth");
-var bookRouter = require("./book");
 var rewardRouter = require("./reward");
 
 var app = express();
 
 app.use("/auth/", authRouter);
-app.use("/book/", bookRouter);
 app.use("/reward/", rewardRouter);
 
 module.exports = app;
